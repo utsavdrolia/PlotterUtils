c986603f36f16588495c4f688000e5e2b32d4ef0
Utsav Drolia <utsavdrolia@gmail.com>
2016-09-02 10:21:06 -0400

    Lineplot supports multiple lines.

--- dual_yaxis_lineplot.py
+++ dual_yaxis_lineplot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
 
-def dual_yaxis_plot(y1, y2, x, y1label, y2label, xlabel, y1lim, y2lim, title, legend, flag):
+def dual_yaxis_plot(y1, y2, x, y1label="", y2label="", xlabel="", y1lim=None, y2lim=None, title="", legend=None, flag=False):
     """
     :param ticks: Labels for ticks on x axis
     :param xlabel: Label for x axis
@@ -22,33 +22,42 @@ def dual_yaxis_plot(y1, y2, x, y1label, y2label, xlabel, y1lim, y2lim, title, le
 
     # cont_y1 = func_y1(ticks)
     # cont_y2 = func_y2(ticks)
-    y1, std_y1 = zip(*y1)
-    y2, std_y2 = zip(*y2)
-    y1 = list(y1)
-    y2 = list(y2)
-    std_y1 = list(std_y1)
-    std_y2 = list(std_y2)
-    print len(y1)
-    print len(std_y1)
+    std_y1 = None
+    std_y2 = None
+    if isinstance(y1[0], tuple):
+        y1, std_y1 = zip(*y1)
+        y1 = list(y1)
+        std_y1 = list(std_y1)
+
+    if isinstance(y2[0], tuple):
+        y2, std_y2 = zip(*y2)
+        y2 = list(y2)
+        std_y2 = list(std_y2)
+
     fig, ax1 = plt.subplots(figsize=(10.24, 5.12))
-    plot1 = ax1.errorbar(x, y1, yerr=std_y1, color='g', fmt='--o', linewidth=2.0)
+    plot1 = ax1.errorbar(x, y1, yerr=std_y1, color='g', linewidth=2.0, label="x1")
     ax1.set_ylabel(y1label)
-    ax1.set_ylim(y1lim)
     ax1.set_xlabel(xlabel)
 
+    if y1lim:
+        ax1.set_ylim(y1lim)
+
     ax2 = ax1.twinx()
-    plot2 = ax2.errorbar(x, y2, yerr=std_y2, color='b', fmt='-.^', linewidth=2.0)
+    plot2 = ax2.errorbar(x, y2, yerr=std_y2, color='b', linewidth=2.0, label="x2")
     ax2.set_ylabel(y2label)
-    ax2.set_ylim(y2lim)
+
+    if y2lim:
+        ax2.set_ylim(y2lim)
 
     xmin = (3 * x[0] - x[1]) / 2.
     xmax = (3 * x[-1] - x[-2]) / 2.
     plt.xlim(xmin, xmax)
-    plt.legend((plot1[0], plot2[0]), legend, loc=0)
-    plt.title(title)
 
+    if legend:
+        plt.legend((plot1[0], plot2[0]), legend, loc=0)
     if flag is True:
         pdf = PdfPages(title + ".pdf")
         plt.savefig(pdf, format='pdf', bbox_inches='tight')
         pdf.close()
     plt.show()
+    return ax1, plt
--- lineplot.py
+++ lineplot.py
@@ -1,6 +1,7 @@
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 
 def lineplot(cr, di, ticks, xlabel, ylabel, title, flag, saveas ,cr_legend, di_legend):
@@ -48,3 +49,38 @@ def lineplot(cr, di, ticks, xlabel, ylabel, title, flag, saveas ,cr_legend, di_l
         pdf.close()
     else:
         plt.show()
+
+
+def lineplot_multi(xarr, ticks, xlim=None, ylim=None, xlabel="", ylabel="", legend="", title="", flag=False, savepath=""):
+    #colors = [(0.5, 0.5, .8), (0.5, .8, 0.5), (.8, 0.5, 0.5), (0.5, .8, .8), (0.8, .8, 0.5), (.8, .8, .8)]
+    #count = 0
+    linestyles = ['-', '--', '-.', ':']
+    c = 0
+    fig, ax = plt.subplots(figsize=(8, 4))
+
+    for x in xarr:
+        x_stds = 0
+        x_means = x
+        if isinstance(x[0], tuple):
+            x_means, x_stds = zip(*x)
+            x_means = list(x_means)
+            x_stds = list(x_stds)
+        plot1 = ax.errorbar(ticks, x_means, linestyle=linestyles[c], yerr=x_stds, linewidth=2, elinewidth=1)
+        c += 1
+
+    if xlim is None:
+        xlim = [(3 * ticks[0] - ticks[1]) / 2., (3 * ticks[-1] - ticks[-2]) / 2.]
+        # shaft half a step to the right
+
+    plt.xlim(xlim[0], xlim[1])
+
+    if ylim is not None :
+        plt.ylim(ylim[0], ylim[1])
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.legend(legend, loc=0)
+    if flag is True:
+        pdf = PdfPages(savepath + os.sep + title + ".pdf")
+        plt.savefig(pdf, format='pdf', bbox_inches='tight')
+        pdf.close()
+    plt.show()
